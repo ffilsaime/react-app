d3d2c083c706f90bef6b87d2c33e741faccadad4
Florebencia <florebencia.filsaime1998@gmail.com>
2025-12-13 15:01:27 -0500

    Made A Message Component And Added Bootstrap to The Project

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,9 @@
 // PascalCasing = each word is capitalized unlike camelcase where the first word has a lowercase letter
 function Message() {
     // the code I have below is JSX: JavaScript XML
+    const name = 'Flo';
+    if (name)
+        return <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>;
 }
 
