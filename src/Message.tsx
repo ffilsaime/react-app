21a7740563bec6790349427e8f4fc385d15c21f9
Florebencia <florebencia.filsaime1998@gmail.com>
2025-12-13 10:32:03 -0500

    Starting To Teach Myself React

---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+// PascalCasing = each word is capitalized unlike camelcase where the first word has a lowercase letter
+function Message() {
+    // the code I have below is JSX: JavaScript XML
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
