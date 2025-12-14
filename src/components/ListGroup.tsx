6fce544a0a75af87f71e4025b34393876323c803
Florebencia <florebencia.filsaime1998@gmail.com>
2025-12-13 22:01:07 -0500

    Learned Conditionals in React

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,7 +3,7 @@ function ListGroup(){
     // return both a ul and a h1
     // you can avoid that by using a div
     // but a better way of doing it is using a fragment
-    const items = [
+    let items = [
         'New York',
         'San Francisco',
         'Tokyo',
@@ -11,9 +11,27 @@ function ListGroup(){
         'Paris'
     ];
 
+    items = [];
+
+    // if (items.length === 0)
+    //     return (
+    //         <>
+    //             <h1>List</h1>
+    //             <p>No item found</p>
+    //         </>
+    //     );
+
+    // const message = items.length === 0 ? <p>No item found</p> : null;
+
+    const getMessage = () => {
+        // return items.length === 0 ? <p>No item found</p> : null;
+        return items.length === 0 && <p>No item found</p>;
+    }
+
     return (
         <>
             <h1>List</h1>
+            { getMessage() }
             <ul className="list-group">
                 {items.map(item => <li className="list-group-item" key={item}>{item}</li>)}
             </ul>
