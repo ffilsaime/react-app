b4aecb486a803b31a0e7ece70de072d6e9c5483b
Florebencia <florebencia.filsaime1998@gmail.com>
2025-12-13 22:14:48 -0500

    Starting to Add Event Handling
    - In parameters of a function for a list did you know that you can add index?

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -11,29 +11,28 @@ function ListGroup(){
         'Paris'
     ];
 
-    items = [];
-
-    // if (items.length === 0)
-    //     return (
-    //         <>
-    //             <h1>List</h1>
-    //             <p>No item found</p>
-    //         </>
-    //     );
-
-    // const message = items.length === 0 ? <p>No item found</p> : null;
-
     const getMessage = () => {
         // return items.length === 0 ? <p>No item found</p> : null;
         return items.length === 0 && <p>No item found</p>;
     }
+
+    // const clickFunction = (value: string) => {
+    //     console.log(value);
+    // }
 
     return (
         <>
             <h1>List</h1>
             { getMessage() }
             <ul className="list-group">
-                {items.map(item => <li className="list-group-item" key={item}>{item}</li>)}
+                { items.map((item, index) =>
+                    <li className="list-group-item"
+                        key={item}
+                        onClick={() => console.log(item, index)}>
+                        {item}
+                    </li>
+                    )
+                }
             </ul>
         </>
     );
