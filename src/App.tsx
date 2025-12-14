7c4b95eaa4b9d84218dff3bfe30ac859fd710aed
Florebencia <florebencia.filsaime1998@gmail.com>
2025-12-14 16:37:44 -0500

    An example of Passing Data Through Properties

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import ListGroup from './components/ListGroup';
 
 function App(){
     //NOTE: You should always close your react component or you'll get a compilation error
-    return <div><ListGroup /></div>;
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    return (
+        <div>
+            <ListGroup items={items} heading='Cities'/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,12 @@
 import { useState } from "react";
 
-function ListGroup(){
-    // Something to note about react: it can't return more than one element like having this class
-    // return both a ul and a h1
-    // you can avoid that by using a div
-    // but a better way of doing it is using a fragment
-    let items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ];
+// {items: [], heading: string}
+interface ListGroupProps {
+    items: string[];
+    heading: string;
+}
 
-    // this is a hook
-   // arr[0] // contains the variable value itself (selected Index)
-    //arr[1] // updater function
+function ListGroup({items, heading}: ListGroupProps){
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
     const getMessage = () => {
@@ -25,7 +16,7 @@ function ListGroup(){
 
     return (
         <>
-            <h1>List</h1>
+            <h1>{heading}</h1>
             { getMessage() }
             <ul className="list-group">
                 { items.map((item, index) =>
