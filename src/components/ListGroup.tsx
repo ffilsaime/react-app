2c57be99d32527120ab9b29ca503cfc16abde5e8
Florebencia <florebencia.filsaime1998@gmail.com>
2025-12-14 15:57:15 -0500

    Learned How to Manage State

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,4 @@
-import type {MouseEvent} from "react";
+import { useState } from "react";
 
 function ListGroup(){
     // Something to note about react: it can't return more than one element like having this class
@@ -13,27 +13,25 @@ function ListGroup(){
         'Paris'
     ];
 
+    // this is a hook
+   // arr[0] // contains the variable value itself (selected Index)
+    //arr[1] // updater function
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
     const getMessage = () => {
         // return items.length === 0 ? <p>No item found</p> : null;
         return items.length === 0 && <p>No item found</p>;
     }
-
-    // const clickFunction = (value: string) => {
-    //     console.log(value);
-    // }
-
-    // event handler
-    const handleClick = (event: MouseEvent) => console.log(event);
 
     return (
         <>
             <h1>List</h1>
             { getMessage() }
             <ul className="list-group">
-                { items.map((item) =>
-                    <li className="list-group-item"
+                { items.map((item, index) =>
+                    <li className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
                         key={item}
-                        onClick={handleClick}>
+                        onClick={ () => {setSelectedIndex(index)}}>
                         {item}
                     </li>
                     )
